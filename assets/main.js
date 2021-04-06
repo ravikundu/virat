62935468727b59307440c834d8b9069a00402861
ravikundu <32717330+ravikundu@users.noreply.github.com>
2021-04-06 13:20:46 +0530

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,165 @@
+function getSupportedVideoFormats() {
+
+    // Test from Modernizr.
+    var elem = document.createElement('video');
+    var bool = false;
+
+    // IE9 Running on Windows Server SKU can cause an exception to be thrown, bug #224
+    try {
+        if (bool = !!elem.canPlayType) {
+            bool = new Boolean(bool);
+            bool.ogg = elem.canPlayType('video/ogg; codecs="theora"').replace(/^no$/, '');
+
+            // Without QuickTime, this value will be `undefined`. github.com/Modernizr/Modernizr/issues/546
+          
+            bool.webm = elem.canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/, '');
+
+            bool.vp9 = elem.canPlayType('video/webm; codecs="vp9"').replace(/^no$/, '');
+
+            bool.hls = elem.canPlayType('application/x-mpegURL; codecs="avc1.42E01E"').replace(/^no$/, '');
+            
+            bool.h264 = elem.canPlayType('video/mp4; codecs="avc1.42E01E"').replace(/^no$/, '');
+
+        }
+    } catch (e) {}
+
+    return bool;
+
+}
+
+function getVideoFormat(mime) {
+
+    return mime ? 'video/webm' : 'webm';
+
+    var bool = getSupportedVideoFormats();
+    // Prioritization of video format fallback.
+    if (bool.h264 !== '') {
+        return mime ? 'video/mp4' : 'mp4';
+    }
+    if (bool.webm !== '') {
+        return mime ? 'video/webm' : 'webm';
+    }
+    if (bool.ogg !== '') {
+        return mime ? 'video/ogg' : 'ogv';
+    }
+}
+
+function updateProgressBar(value) {
+    value = Math.round(value);
+    progress.setAttribute('valuenow', value);
+    progress.style.width = value + '%';
+    progress.innerHTML = value + '%';
+}
+
+function updateProgressBarG(value) {
+    value = Math.round(value);
+    progressg.setAttribute('valuenow', value);
+    progressg.style.width = value + '%';
+    progressg.innerHTML = value + '%';
+}
+
+redloaded = 'no';
+greenloaded = 'no';
+
+function loadVideoFully(event) {
+
+    GET(url);
+
+    function onProgress(event) {
+        if (event.lengthComputable) {
+            var completion = (event.loaded / event.total) * 100;
+        }
+    }
+
+    function onLoad(event) {
+        var type = getVideoFormat(true);
+        var blob = new Blob([event.target.response], {
+            type: type
+        });
+        document.getElementById("video").type = type;
+        document.getElementById("video").src = URL.createObjectURL(blob);
+        
+        redloaded='yes';
+        if(greenloaded=='yes')
+        {
+        document.getElementById("loading").style.visibility = "hidden";
+        }
+            
+        }
+
+    function GET(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+        xhr.responseType = 'arraybuffer';
+        xhr.onprogress = onProgress;
+        xhr.onload = onLoad;
+        xhr.send();
+    }
+
+}
+
+// Video sample from videojs, without extension.
+var url = 'video/redvideo91nov.' + getVideoFormat();
+var video = document.getElementById('video');
+
+// Video Green Variables
+var urlg = 'video/greenvideo4nov.' + getVideoFormat();
+var videog = document.getElementById('video2');
+
+
+
+function loadVideoFullyG(event) {
+
+    GET(urlg);
+
+    function onProgress(event) {
+        if (event.lengthComputable) {
+            var completion = (event.loaded / event.total) * 100;
+         }
+    }
+
+    function onLoad(event) {
+        var type = getVideoFormat(true);
+        var blob = new Blob([event.target.response], {
+            type: type
+        });
+        
+        console.log(type);
+        document.getElementById("video2").type = type;
+        document.getElementById("video2").src = URL.createObjectURL(blob);
+        
+        greenloaded='yes';
+        if(redloaded=='yes')
+        {
+        document.getElementById("loading").style.visibility = "hidden";
+        }
+        
+    }
+
+    function GET(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+        xhr.responseType = 'arraybuffer';
+        xhr.onprogress = onProgress;
+        xhr.onload = onLoad;
+        xhr.send();
+    }
+
+}
+
+
+
+
+
+function loadVideo(event) 
+{
+   loadVideoFully(event);
+   loadVideoFullyG(event);
+}
+
+//body.addEventListener("load", loadVideo(), false);
+//button.addEventListener('click', loadVideo);
+
+console.debug('Video Formats Supported:', getSupportedVideoFormats());
